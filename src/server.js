4f6b948c587819bccba53b2627069e1507bb60f7
Srijith vy <srijithviswanathan@gmail.com>
2024-11-20 14:24:58 +0530

    deployed

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
-// export const server_API = "https://server-blm.onrender.com/api/v1/blood";
-// export const server_API1 = "https://server-blm.onrender.com/api/v1/user";
-export const server_API = "http://localhost:3333/api/v1/blood";
-export const server_API1 = "http://localhost:3333/api/v1/user";
-export const server_API2 = "http://localhost:3333/api/v1/donar";
+export const server_API = "https://redconnect.onrender.com/api/v1/blood";
+export const server_API1 = "https://redconnect.onrender.com/api/v1/user";
+export const server_API2 = "https://redconnect.onrender.com/api/v1/donar";
+// export const server_API = "http://localhost:3333/api/v1/blood";
+// export const server_API1 = "http://localhost:3333/api/v1/user";
+// export const server_API2 = "http://localhost:3333/api/v1/donar";
